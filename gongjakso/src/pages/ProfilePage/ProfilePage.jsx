594da96bb3049a9ee46d56856884d1f31eb084d9
sxxcxng <subin3620@naver.com>
2024-01-18 18:23:22 +0900

    :lipstick: Add Mypage profile 스타일 수정
    #3 Add Mypage profile 스타일 수정

--- gongjakso/src/pages/ProfilePage/ProfilePage.jsx
+++ gongjakso/src/pages/ProfilePage/ProfilePage.jsx
@@ -1,18 +1,8 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
-import styled from 'styled-components'
-import nextIcon from '../../assets/images/myPageNext.svg';
-import myPagePortfolio from '../../assets/images/myPagePortfolio.svg';
-import myPageProfile from '../../assets/images/myPageProfile.svg';
-
-
-let DetailBox = styled.div`
-    position: relative;
-    top: 75px;
-    padding-right: 30px;
-    line-height:5px;
-    text-align: right
-`;
+import styled from 'styled-components';
+import Arrow from '../../assets/images/Arrow.svg';
+import My_page_big from '../../assets/images/My_page_big.svg';
 
 let TopBox = styled.div`
     height: 250px;
@@ -20,32 +10,60 @@ let TopBox = styled.div`
     background-color: rgba(195, 233, 255, 0.5);
     display: flex;
     justify-content: center;
-    align-items: flex-end
+    align-items: flex-end;
 `;
 
+let DetailBox = styled.div`
+    position: relative;
+    transform: translateY(150%);
+    padding-right: 30px;
+    line-height: 25px;
+    text-align: right;
+`;
+
+//나의 포트폴리오 파란색 박스
+let PortfolioBox = styled.button`
+    width: 150px;
+    height: 40px;
+    background-color: #0054ff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #ffffff;
+    transform: translateY(150%);
+    border-radius: 25px;
+    margin-left: 30px;
+`;
 let TopDetail = styled.div`
     padding: 10px 200px;
-    padding-top: 150px
+    padding-top: 150px;
 `;
 
 let GlobalDetail = styled.div`
     padding: 10px 200px;
     display: flex;
-    alignItems: center
+    align-items: center;
 `;
-
 
 const ProfilePage = () => {
     return (
         <div>
             <TopBox>
-            <DetailBox>
-                <h3>최수빈</h3>
-                <h5>공학/소프트웨어학</h5>
-            </DetailBox>
-            <img src={myPageProfile} alt="myPageProfile" style={{width: '150px', marginRight: '10px', transform: 'translateY(50%)'}} />
+                <DetailBox>
+                    <h3>최수빈</h3>
+                    <h5>공학/소프트웨어학</h5>
+                </DetailBox>
+                <img
+                    src={My_page_big}
+                    alt="My_page_big"
+                    style={{
+                        width: '150px',
+                        marginRight: '10px',
+                        transform: 'translateY(50%)',
+                    }}
+                />
                 <Link to="/teamPortfolio">
-                    <img src={myPagePortfolio} alt="myPagePortfolio" style={{width: '150px', transform: 'translateY(150%)'}} />
+                    <PortfolioBox>나의 포트폴리오</PortfolioBox>
                 </Link>
             </TopBox>
             <TopDetail>
@@ -54,20 +72,29 @@ const ProfilePage = () => {
 
             <GlobalDetail>
                 <h3>내가 지원한 팀</h3>
-                <Link to="/teamSupport" style={{margin:"17px"}}>
-                    <img src={nextIcon} alt="myPageNext" width="50px" height="22px" />
+                <Link to="/teamSupport">
+                    <img
+                        src={Arrow}
+                        alt="myPageNext"
+                        width="50px"
+                        height="22px"
+                    />
                 </Link>
             </GlobalDetail>
 
             <GlobalDetail>
                 <h3>내가 참여한 공모전/프로젝트</h3>
-                <Link to="/teamPart" style={{margin:"17px"}}>
-                    <img src={nextIcon} alt="myPageNext" width="50px" height="22px" />
+                <Link to="/teamPart">
+                    <img
+                        src={Arrow}
+                        alt="myPageNext"
+                        width="50px"
+                        height="22px"
+                    />
                 </Link>
-
             </GlobalDetail>
         </div>
-    )
-}
+    );
+};
 
 export default ProfilePage;
--- gongjakso/src/pages/ProfilePage/TeamPortfolio.jsx
+++ gongjakso/src/pages/ProfilePage/TeamPortfolio.jsx
@@ -1,21 +1,55 @@
-import React from 'react'
-import myPagePortfolioCheck from '../../assets/images/myPagePortfolioCheck.svg';
+import React from 'react';
+import styled from 'styled-components';
+
+let BottomBox = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-bottom: 1px solid #b7b7b7;
+    width: 30%;
+    margin: auto;
+`;
+
+//이메일 작성 후 확인 버튼
+let CheckBox = styled.button`
+    width: 100px;
+    cursor: pointer;
+    position: relative;
+    top: 40px;
+    border: 1px solid #b7b7b7;
+    background-color: transparent;
+    color: black;
+    border-radius: 7px;
+`;
 
 const TeamPortfolio = () => {
     return (
-        <div style={{textAlign: 'center'}}>
+        <div style={{ textAlign: 'center' }}>
             <h1>나의 포트폴리오</h1>
             <br></br>
-            <h2>다양한 포트폴리오 양식을 구매하고,<br></br>선배들의 조언까지!</h2>
+            <h2>
+                다양한 포트폴리오 양식을 구매하고,<br></br>선배들의 조언까지!
+            </h2>
             <br></br>
-            <h2 style={{color: '#0054FF'}}>포트폴리오 탭<br></br>Coming Soon</h2>
+            <h2 style={{ color: '#0054FF' }}>
+                포트폴리오 탭<br></br>Coming Soon
+            </h2>
             <br></br>
-            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', borderBottom: '1px solid #B7B7B7', width: '30%', margin: 'auto'}}>
-                <input type="email" placeholder="*포트폴리오 탭을 가장 먼저 만나보고 싶다면 메일 주소를 남겨주세요!" style={{border: 'none', outline: 'none', backgroundColor: 'transparent', width: '70%'}} />
-                <img src={myPagePortfolioCheck} alt="Check" style={{marginLeft: 'auto', cursor: 'pointer', position: 'relative', top: '60px'}} />
-            </div>
+            <BottomBox>
+                <input
+                    type="email"
+                    placeholder="*포트폴리오 탭을 가장 먼저 만나보고 싶다면 메일 주소를 남겨주세요!"
+                    style={{
+                        border: 'none',
+                        outline: 'none',
+                        backgroundColor: 'transparent',
+                        width: '100%',
+                    }}
+                />
+                <CheckBox>확인</CheckBox>
+            </BottomBox>
         </div>
-    )
-}
+    );
+};
 
 export default TeamPortfolio;
