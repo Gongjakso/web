b2368fe33fbae4266d8b0b987eaa37f7d24e9603
chaeyoungwon <epopcy@naver.com>
2024-01-18 17:22:57 +0900

    :lipstick: Edit homepage Text 변경
    #4 homepage text 변경

--- gongjakso/src/pages/HomePage/HomePage.jsx
+++ gongjakso/src/pages/HomePage/HomePage.jsx
@@ -5,104 +5,190 @@ import calendarImage from '../../assets/images/calendar.png';
 import teamBuildingImage from '../../assets/images/teambuilding.png';
 
 const HomePage = () => {
-  const { goToPage } = useCustomNavigate();
-  const handleButtonClick = (path) => {
-    goToPage(path);
-  };
+    const { goToPage } = useCustomNavigate();
+    const handleButtonClick = path => {
+        goToPage(path);
+    };
 
-  return (
-    <>
-      <S.HomeContent>
-        <h2 style ={{letterSpacing:'-2px'}}> 공모전/프로젝트, 어떻게 시작해야할지 막막하셨나요? </h2>
-        <h2 style ={{marginTop: '0.5px', letterSpacing:'-2px'}}> 공작소에서 원하는 사람들과 시작해보세요 !</h2>
-        
-        <div style={{marginTop: '20px', marginBottom: '20px'}}> 공모전 슬라이드 배너 </div>
+    return (
+        <>
+            <S.HomeContent>
+                <S.Title>
+                    공모전/프로젝트, 어떻게 시작해야할지 막막하셨나요?{' '}
+                </S.Title>
+                <S.Title>공작소에서 원하는 사람들과 시작해보세요 !</S.Title>
 
-        <S.BoxContainer>
-          <S.Box> <div> 😔 공모전은 처음인데 어떻게 시작해야 하나요 </div> </S.Box>
-          <S.Box> <div> 😔 학교 밖 사람들과 프로젝트를 해보고 싶어요 </div> </S.Box>
-        
-        </S.BoxContainer>
-        <S.BoxContainer>
-            <S.Box> <div> 🙁 친구랑 같이 하기엔 놀기만 할 것 같아요 </div> </S.Box>
-            <S.Box> <div> 🤯 사이드 프로젝트를 찾고 싶어요 </div> </S.Box>
-        </S.BoxContainer>
+                <div style={{ marginTop: '20px', marginBottom: '20px' }}>
+                    공모전 슬라이드 배너
+                </div>
 
-        <S.Button>
-          <S.Button1 onClick={() => handleButtonClick('/contest')}> 공모전 공고 바로가기 </S.Button1>
-          <S.Button2 style={{marginLeft: '30px'}} onClick={() => handleButtonClick('/project')}> 프로젝트 공고 바로가기 </S.Button2>
-        </S.Button>
-      </S.HomeContent>
-    
-      <S.HomeContent1>
-        <h2 style={{color: 'white', letterSpacing:'-2px', marginTop: '150px'}}>📍다양한 유형의 팀빌딩 서비스 지원</h2>
-        <S.Wrapper>
-          <p> 로그인 후 '팀 빌딩' 탭을 들어가면 팀장으로 팀을 꾸릴 수 있습니다! </p>
-          <p> 오프라인 회의를 선호하는 이들을 위해서 시/군/구의 정보를 선택하여 해당 지역에서 회의하는 공모전 팀을 모아볼 수 있습니다.</p>
-          <S.ImageBox>
-            <div>
-              <S.Box style={{backgroundColor:'#CCCCCC', width: 120, borderRadius:0, height: 120}}>
-                3D 이미지
-              </S.Box>
-              <p>팀장으로 모집하기</p>
-            </div>
-            <div>
-              <img src={teamBuildingImage} alt='팀원 지원 이미지' style={{width: 100, height:'auto', marginTop: '20px', marginBottom: '20px'}} />
-              <p>팀원으로 지원하기</p>
-            </div>
-            <div>
-              <S.Box style={{backgroundColor:'#CCCCCC', width: 120, borderRadius:0, height: 120}}>
-                3D 이미지
-              </S.Box>
-              <p>지역으로 팀 찾기</p>
-            </div>
-          </S.ImageBox>
-        </S.Wrapper>
-        <S.Button1 onClick={() => handleButtonClick('/teambuilding')}> 팀빌딩 바로가기 </S.Button1>
-      </S.HomeContent1>
+                <S.BoxContainer>
+                    <S.Box>
+                        <div>😔 공모전은 처음인데 어떻게 시작해야 하나요</div>
+                    </S.Box>
+                    <S.Box>
+                        <div>😔 학교 밖 사람들과 프로젝트를 해보고 싶어요</div>
+                    </S.Box>
+                </S.BoxContainer>
+                <S.BoxContainer>
+                    <S.Box>
+                        <div>🙁 친구랑 같이 하기엔 놀기만 할 것 같아요</div>
+                    </S.Box>
+                    <S.Box>
+                        <div> 🤯 사이드 프로젝트를 찾고 싶어요 </div>
+                    </S.Box>
+                </S.BoxContainer>
 
-      <S.HomeContent2>
-        <p style={{color: 'white', letterSpacing:'-1px', marginTop: '180px'}}>팀빌딩 서비스만 가능한가요? 아닙니다!🙅🏻</p>
-        <h2 style={{color: 'white', letterSpacing:'-1px', marginTop: '10px'}}>내가 스크랩한 공모전을 📆캘린더로 모아볼 수 있어요.</h2>
-        
-        <S.Container>
-          <S.WhiteBox>
-            <S.Image src={calendarImage} alt="캘린더 이미지"/>
-          </S.WhiteBox>
-          
-          <div style={{color:'white', letterSpacing: '-1px', fontWeight: 500, fontSize: 14}}>
-            <p>내가 스크랩한 공모전/프로젝트 정보를 캘린더로 모아볼 수 있습니다.</p>
-            <p>여러 가지 정보가 기재되어 있는 다른 캘린더들과 달리</p>
-            <p> 공모전/프로젝트 정보를 모아두고</p>
-            <p>관련한 할 일들을 정리할 수 있어요.</p>
+                <S.Button>
+                    <S.Button1 onClick={() => handleButtonClick('/contest')}>
+                        공모전 공고 바로가기
+                    </S.Button1>
+                    <S.Button2
+                        style={{ marginLeft: '30px' }}
+                        onClick={() => handleButtonClick('/project')}
+                    >
+                        프로젝트 공고 바로가기
+                    </S.Button2>
+                </S.Button>
+            </S.HomeContent>
 
-            <S.Button2 style={{marginTop: '80px'}} onClick={() => handleButtonClick('/calendar')}> 캘린더 바로가기 </S.Button2>
-          </div>
-        </S.Container>        
-      </S.HomeContent2>
+            <S.HomeContent1>
+                <S.Title1>📍다양한 유형의 팀빌딩 서비스 지원</S.Title1>
 
-      <S.HomeContent3>
-        <p style={{color: '#0150F1', letterSpacing:'-1px', marginTop: '150px', fontWeight: 'bold', fontSize: 20}}>Coming Soon</p>
-        <p style={{color: 'black', letterSpacing:'-1px', marginTop: '10px'}}>공모전/프로젝트의 사후 관리도 공작소에서 만나보세요!😉</p>
-        <h2 style={{color: 'black', letterSpacing:'-2px', marginTop: '10px'}}>마이페이지의 '포트폴리오' 탭에서 다양한 템플릿을 제공해드려요.</h2>
-        
-        <S.Container>
-          
-          <div style={{color:'black', letterSpacing: '-1px', fontWeight: 500, fontSize: 14}}>
-            <p>노션? pdf? 어떤 툴을 써야하는지도,</p>
-            <p>어떤 내용을 채워야하는지도 감이 안오셨다고요?</p>
-            <p>공작소에서는 공작가 여러분들이 열심히 쌓아주신</p>
-            <p>포트폴리오를 정리하는 것을 돕기 위해</p>
-            <p>다양한 형태의 템플릿을 제공해드립니다.</p>
-            <S.Button3 onClick={() => handleButtonClick('/mail')}> 출시 메일 받으러 가기 </S.Button3>
-          </div>
-          <S.WhiteBox style={{backgroundColor:'#CCCCCC'}}>
-            3D 이미지
-          </S.WhiteBox>
-        </S.Container>        
-      </S.HomeContent3>
-    </>
-  );
+                <S.Wrapper>
+                    <S.Detail1>
+                        로그인 후 '팀 빌딩' 탭을 들어가면 팀장으로 팀을 꾸릴 수
+                        있습니다!{' '}
+                    </S.Detail1>
+                    <S.Detail1>
+                        오프라인 회의를 선호하는 이들을 위해서 시/군/구의 정보를
+                        선택하여 해당 지역에서 회의하는 공모전 팀을 모아볼 수
+                        있습니다.
+                    </S.Detail1>
+                    <S.ImageBox>
+                        <div>
+                            <S.Box
+                                style={{
+                                    backgroundColor: '#CCCCCC',
+                                    width: 120,
+                                    borderRadius: 0,
+                                    height: 120,
+                                }}
+                            >
+                                3D 이미지
+                            </S.Box>
+                            <p>팀장으로 모집하기</p>
+                        </div>
+                        <div>
+                            <img
+                                src={teamBuildingImage}
+                                alt="팀원 지원 이미지"
+                                style={{
+                                    width: 100,
+                                    height: 'auto',
+                                    marginBottom: '15px',
+                                }}
+                            />
+                            <p>팀원으로 지원하기</p>
+                        </div>
+                        <div>
+                            <S.Box
+                                style={{
+                                    backgroundColor: '#CCCCCC',
+                                    width: 120,
+                                    borderRadius: 0,
+                                    height: 120,
+                                }}
+                            >
+                                3D 이미지
+                            </S.Box>
+                            <p>지역으로 팀 찾기</p>
+                        </div>
+                    </S.ImageBox>
+                </S.Wrapper>
+                <S.Button1 onClick={() => handleButtonClick('/teambuilding')}>
+                    팀빌딩 바로가기
+                </S.Button1>
+            </S.HomeContent1>
+
+            <S.HomeContent2>
+                <S.Subtitle2>
+                    팀빌딩 서비스만 가능한가요? 아닙니다!🙅🏻
+                </S.Subtitle2>
+
+                <S.Title2>
+                    내가 스크랩한 공모전을 📆캘린더로 모아볼 수 있어요.
+                </S.Title2>
+
+                <S.Container>
+                    <S.WhiteBox>
+                        <S.Image src={calendarImage} alt="캘린더 이미지" />
+                    </S.WhiteBox>
+
+                    <div style={{ padding: '10px' }}>
+                        <S.Detail2>
+                            내가 스크랩한 공모전/프로젝트 정보를 캘린더로 모아볼
+                            수 있습니다.
+                        </S.Detail2>
+                        <S.Detail2>
+                            여러 가지 정보가 기재되어 있는 다른 캘린더들과 달리
+                        </S.Detail2>
+                        <S.Detail2>공모전/프로젝트 정보를 모아두고</S.Detail2>
+                        <S.Detail2>
+                            관련한 할 일들을 정리할 수 있어요.
+                        </S.Detail2>
+
+                        <S.Button2
+                            style={{ marginTop: '70px' }}
+                            onClick={() => handleButtonClick('/calendar')}
+                        >
+                            캘린더 바로가기
+                        </S.Button2>
+                    </div>
+                </S.Container>
+            </S.HomeContent2>
+
+            <S.HomeContent3>
+                <S.Subtitle3>Coming Soon</S.Subtitle3>
+                <S.Detail3>
+                    공모전/프로젝트의 사후 관리도 공작소에서 만나보세요!😉
+                </S.Detail3>
+
+                <S.Title3>
+                    마이페이지의 '포트폴리오' 탭에서 다양한 템플릿을
+                    제공해드려요.
+                </S.Title3>
+
+                <S.Container>
+                    <div>
+                        <S.Detail3>
+                            노션? pdf? 어떤 툴을 써야하는지도,
+                        </S.Detail3>
+                        <S.Detail3>
+                            어떤 내용을 채워야하는지도 감이 안오셨다고요?
+                        </S.Detail3>
+                        <S.Detail3>
+                            공작소에서는 공작가 여러분들이 열심히 쌓아주신
+                        </S.Detail3>
+                        <S.Detail3>
+                            포트폴리오를 정리하는 것을 돕기 위해
+                        </S.Detail3>
+                        <S.Detail3>
+                            다양한 형태의 템플릿을 제공해드립니다.
+                        </S.Detail3>
+
+                        <S.Button3 onClick={() => handleButtonClick('/mail')}>
+                            출시 메일 받으러 가기
+                        </S.Button3>
+                    </div>
+
+                    <S.WhiteBox style={{ backgroundColor: '#CCCCCC' }}>
+                        3D 이미지
+                    </S.WhiteBox>
+                </S.Container>
+            </S.HomeContent3>
+        </>
+    );
 };
 
 export default HomePage;
--- gongjakso/src/pages/HomePage/HomePageStyled.jsx
+++ gongjakso/src/pages/HomePage/HomePageStyled.jsx
@@ -1,11 +1,71 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 import backGroundImage from '../../assets/images/background.png';
+
+export const Title = styled.p`
+    font-size: 25px;
+    text-align: center;
+    padding: 5px;
+    font-weight: bold;
+`;
+
+export const Title1 = styled.div`
+    color: white;
+    font-size: 25px;
+    font-weight: bold;
+    margintop: 150px;
+`;
+
+export const Title2 = styled.div`
+    color: white;
+    padding: 10px;
+    font-size: 20px;
+    font-weight: bold;
+`;
+
+export const Title3 = styled.div`
+    color: black;
+    padding: 10px;
+    font-size: 20px;
+    font-weight: bold;
+`;
+
+export const Subtitle2 = styled.div`
+    color: white;
+    margin-top: 180px;
+`;
+
+export const Subtitle3 = styled.div`
+    color: #0150f1;
+    align-items: center;
+    font-weight: bold;
+    padding: 10px;
+    font-size: 20px;
+`;
+
+export const Detail1 = styled.p`
+    font-size: 14px;
+    padding: 3px;
+    color: white;
+`;
+
+export const Detail2 = styled.p`
+    font-size: 14px;
+    padding: 10px;
+    color: white;
+`;
+
+export const Detail3 = styled.p`
+    color: black;
+    padding: 10px;
+    font-size: 14px;
+`;
 
 export const Button = styled.p`
     display: flex;
     margin-top: 50px;
     flex-direction: row;
-`
+`;
+
 export const Button1 = styled.button`
     background-color: black;
     border: none;
@@ -17,7 +77,7 @@ export const Button1 = styled.button`
     width: 300px;
     padding: 18px;
     cursor: pointer;
-`
+`;
 export const Button2 = styled.button`
     background-color: white;
     border: none;
@@ -29,11 +89,10 @@ export const Button2 = styled.button`
     margin-bottom: 50px;
     padding: 18px;
     cursor: pointer;
-    
-`
+`;
 
 export const Button3 = styled.button`
-    background-color: #0150F1;
+    background-color: #0150f1;
     border: none;
     font-size: 16px;
     font-weight: bold;
@@ -45,11 +104,21 @@ export const Button3 = styled.button`
     width: 300px;
     padding: 18px;
     cursor: pointer;
-`
+`;
 
 export const HomeContent = styled.div`
     display: flex;
-    background-image: linear-gradient(to left, rgba(255, 255, 255, 0), rgba(255, 255, 255, 0.1)), linear-gradient(to right, rgba(255, 255, 255, 0), rgba(255, 255, 255, 0)), url(${backGroundImage});
+    background-image: linear-gradient(
+            to left,
+            rgba(255, 255, 255, 0),
+            rgba(255, 255, 255, 0.1)
+        ),
+        linear-gradient(
+            to right,
+            rgba(255, 255, 255, 0),
+            rgba(255, 255, 255, 0)
+        ),
+        url(${backGroundImage});
     background-size: cover;
     width: 100vw;
     height: 100vh;
@@ -57,28 +126,27 @@ export const HomeContent = styled.div`
     justify-content: center;
     flex-direction: column;
     align-items: center;
-`
+`;
 
 export const Box = styled.div`
     color: black;
-    background-color: rgba(255,255,255,0.6);
+    background-color: rgba(255, 255, 255, 0.6);
     border-radius: 30px;
-    border: 2px solid rgba(255,255,255,0.8);
+    border: 2px solid rgba(255, 255, 255, 0.8);
     padding: 18px;
-    width: 400px;
+    width: 350px;
     display: flex;
-    letter-spacing: -1px;
     align-items: center;
     justify-content: center;
-    height: 30px;
+    height: 50px;
     text-align: center;
     margin: 10px;
-`
+`;
 export const BoxContainer = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-around;
-`
+`;
 
 export const HomeContent1 = styled.div`
     display: flex;
@@ -86,30 +154,32 @@ export const HomeContent1 = styled.div`
     align-items: center;
     height: 100vh;
     flex-direction: column;
-    background-color: #0150F1;
-`
+    background-color: #0150f1;
+`;
 
 export const Wrapper = styled.div`
-    background-color: rgba(255,255,255,0.4);
-    padding: 40px;
+    background-color: rgba(255, 255, 255, 0.4);
+    padding: 50px;
     margin-top: 20px;
     margin-bottom: 40px;
     border: 1px solid white;
     border-radius: 30px;
     height: 300px;
     font-size: 15px;
+    padding: 40px;
     text-align: center;
-    letter-spacing: -1px;
     color: white;
-`
+`;
+
 export const ImageBox = styled.div`
     display: flex;
+    padding: 20px;
     justify-content: space-between;
     margin-left: 100px;
     margin-right: 100px;
     text-align: center;
     flex-direction: row;
-`
+`;
 export const HomeContent2 = styled.div`
     display: flex;
     justify-content: center;
@@ -118,11 +188,12 @@ export const HomeContent2 = styled.div`
     weight: 100%;
     height: 100vh;
     background-color: #000000;
-`
+`;
 export const Container = styled.div`
     display: flex;
     flex-direciton: row;
-`
+    padding: 10px;
+`;
 
 export const HomeContent3 = styled.div`
     display: flex;
@@ -131,16 +202,17 @@ export const HomeContent3 = styled.div`
     flex-direction: column;
     weight: 100%;
     height: 100vh;
-    background-color: #FFFFFF;
-`
+    background-color: #ffffff;
+`;
+
 export const WhiteBox = styled.div`
-    background-color: #FFFFFF;
+    background-color: #ffffff;
     width: 350px;
     height: 280px;
     margin: 20px;
     margin-bottom: 150px;
-    border-radius: 20px; 
-    overflow: hidden; 
+    border-radius: 20px;
+    overflow: hidden;
 `;
 
 export const Image = styled.img`
