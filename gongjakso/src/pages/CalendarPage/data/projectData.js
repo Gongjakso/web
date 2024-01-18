5c7dace391e71f9db369c640bf62862a81f15a2a
Hanseul Lee <lhs010107@gmail.com>
2024-01-18 17:36:31 +0900

    :lipstick: Add Calendar UI 구현
    #9 Calendar UI 구현

---
+++ gongjakso/src/pages/CalendarPage/data/projectData.js
@@ -0,0 +1,16 @@
+let projectData = [
+    {
+        title: '프로젝트 마감',
+        color: '#e789ff5a',
+        textColor: '#e789ff',
+        date: '2024-01-08',
+    },
+    {
+        title: '프로젝트 마감',
+        color: '#e789ff5a',
+        textColor: '#e789ff',
+        date: '2024-01-17',
+    },
+];
+
+export default projectData;
