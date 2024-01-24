5e418b577b848aab789b4e487c4cc2da9adf7dc7
chaeyoungwon <epopcy@naver.com>
2024-01-24 23:38:44 +0900

    :lipstick: Add Loginpage UI 구현
    #23 Loginpage UI 구현

---
+++ gongjakso/src/components/Auth/Login.Styled.jsx
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+    cursor: pointer;
+    background: transparent;
+    margin-top: 30px;
+    border: none;
+`;
+export const LoginInfo = styled.div`
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    font-size: 20px;
+    margin-top: 0px;
+    font-weight: bold;
+    letter-spacing: -1px;
+`;
+
+export const LoginContent = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100vh;
+    margin-top: -150px;
+    background: white;
+    flex-direction: row;
+`;
+export const Image = styled.img`
+    width: 300px;
+    height: auto;
+    padding: 50px;
+`;
+
+export const IconImage = styled.img`
+    width: 300px;
+    height: auto;
+    padding: 10px;
+`;
--- gongjakso/src/components/Auth/Login.jsx
+++ gongjakso/src/components/Auth/Login.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import * as S from '../Auth/Login.Styled';
+import kakaoLogin from '../../assets/images/kakao_login.svg';
+import macbookImage from '../../assets/images/macBookImage.svg';
 
 const Login = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    return (
+        <S.LoginContent>
+            <S.LoginInfo>
+                3초만에 로그인하고 <br /> 공모전과 프로젝트를 시작해보세요!
+                <S.Button>
+                    <S.IconImage src={kakaoLogin} alt="카카오로그인" />{' '}
+                </S.Button>
+            </S.LoginInfo>
+            <S.Image src={macbookImage} alt="맥북" />
+        </S.LoginContent>
+    );
+};
 
-export default Login
+export default Login;
