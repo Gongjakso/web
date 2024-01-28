64f1aff9fa2bf44d1acd6794e71b42ab094f2b13
HyeonJin <ocn4091@naver.com>
2024-01-28 19:25:16 +0900

    :sparkles: Add Input, selectInput 컴포넌트 구현
    #26 input, selectInput 컴포넌트 구현

--- gongjakso/src/router/Router.jsx
+++ gongjakso/src/router/Router.jsx
@@ -1,9 +1,16 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import PublicRoutePage from './PublicRoute';
 import PrivateRoutePage from './PrivateRoute';
 import { Suspense } from 'react';
 import HomePage from '../pages/HomePage/HomePage';
 import DefaultLayout from '../Layout/DefaultLayout';
+import ProfilePage from '../pages/ProfilePage/ProfilePage';
+import TeamPart from '../pages/ProfileParticipated/ParticipatedTeam';
+import TeamSupport from '../pages/ProfileApplied/AppliedTeam';
+import TeamPortfolio from '../pages/Portfolio/MyPortfolio';
+import Calendar from '../pages/CalendarPage/Calendar';
+import DetailPageContest from '../pages/DetailPage/DetailPageContest';
+import DetailPageProject from '../pages/DetailPage/DetailPageProject';
 
 const Router = () => {
     return (
@@ -12,7 +19,41 @@ const Router = () => {
                 <Routes>
                     <Route element={<PublicRoutePage />}>
                         <Route element={<DefaultLayout />}>
-                            <Route path={'/home'} element={<HomePage />} />
+                            <Route path={'/'} element={<HomePage />} />
+                            <Route
+                                path={'/contest'}
+                                element={<DetailPageContest />}
+                            />
+                            <Route
+                                path={'/project'}
+                                element={<DetailPageProject />}
+                            />
+                            <Route path={'/calendar'} element={<Calendar />} />
+                            <Route path={'/teambuild'} element={<HomePage />} />
+                            <Route
+                                path={'/profile'}
+                                element={<ProfilePage />}
+                            />
+                            <Route
+                                path={'/appliedTeam'}
+                                element={<TeamSupport />}
+                            />
+                            <Route
+                                path={'/participatedTeam'}
+                                element={<TeamPart />}
+                            />
+                            <Route
+                                path={'/teamPortfolio'}
+                                element={<TeamPortfolio />}
+                            />
+                            <Route
+                                path={'/contest'}
+                                element={<DetailPageContest />}
+                            />
+                            <Route
+                                path={'/project'}
+                                element={<DetailPageProject />}
+                            />
                         </Route>
                     </Route>
                 </Routes>
