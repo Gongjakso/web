f3a92b6d2a89d82fce64d2aeaeff31e6cf2cf630
HyeonJin <ocn4091@naver.com>
2024-02-03 17:02:32 +0900

    :lipstick: Add PostMainPage 파일 추가
    #43 공고 게시물 페이지 구현 시작

---
+++ gongjakso/src/pages/PostMainPage/PostMainPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PostMainPage = () => {
+    return (
+        <>
+            <div>프로젝트/공모전 공고 메인 페이지</div>
+        </>
+    );
+};
+
+export default PostMainPage;
