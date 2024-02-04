2e535b38b35f1566c96fde082d3e81581a5fe0b6
HyeonJin <ocn4091@naver.com>
2024-02-04 11:26:54 +0900

    :lipstick: Add PostPage UI 구현중
    #43 프로젝트/공모전 공고 페이지 배너 구현

--- gongjakso/src/pages/PostMainPage/PostMainPage.jsx
+++ gongjakso/src/pages/PostMainPage/PostMainPage.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import * as S from './PostMainPage.Styled';
+import SwiperBanner from '../../components/SwiperBanner/SwiperBanner';
 
 const PostMainPage = () => {
     return (
         <>
-            <div>프로젝트/공모전 공고 메인 페이지</div>
+            <S.MainContent>
+                <S.Div>
+                    <SwiperBanner banner1={S.Banners} />
+                </S.Div>
+                <div>프로젝트/공모전 공고 메인 페이지</div>
+                <div>프로젝트/공모전 공고 메인 페이지</div>
+                <div>프로젝트/공모전 공고 메인 페이지</div>
+            </S.MainContent>
         </>
     );
 };
