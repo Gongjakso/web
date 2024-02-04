c029c57df6ad35df30d4e7197a93ea54d33b132b
HyeonJin <ocn4091@naver.com>
2024-02-04 11:26:54 +0900

    :lipstick: Add PostPage UI 구현중
    #43 프로젝트/공모전 공고 페이지 배너 구현

--- gongjakso/src/pages/PostMainPage/PostMainPage.Styled.jsx
+++ gongjakso/src/pages/PostMainPage/PostMainPage.Styled.jsx
@@ -1,1 +1,25 @@
 import styled from 'styled-components';
+import { ReactComponent as Banner } from '../../assets/images/banner.svg';
+
+export const MainContent = styled.div`
+    width: 100%;
+    max-width: 1200px;
+    margin: 0 auto;
+    margin-top: 90px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export const Banners = styled(Banner)`
+    svg {
+        width: 100%;
+        height: auto;
+    }
+`;
+
+export const Div = styled.div`
+    width: 1200px;
+    height: 100%;
+    display: flex;
+`;
