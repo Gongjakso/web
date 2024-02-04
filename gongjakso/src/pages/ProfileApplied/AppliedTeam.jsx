bac65e17d7e99a3004dab1f27239d76b0f918005
sxxcxng <subin3620@naver.com>
2024-02-05 02:35:19 +0900

    :lipstick: Add AppliedTeam 페이지 teamBox
    #42 AppliedTeam 페이지 teamBox 추가

--- gongjakso/src/pages/ProfileApplied/AppliedTeam.jsx
+++ gongjakso/src/pages/ProfileApplied/AppliedTeam.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import * as S from './AppliedTeamStyled';
+import TeamBox from '../TeamBox/TeamBox';
+//import { useHistory } from 'react-router-dom';
 
 const TeamSupport = () => {
     return (
         <div>
             <S.TopBox>
+                <S.Spacer />
                 <S.Title>내가 지원한 팀</S.Title>
             </S.TopBox>
+            <S.BoxDetail>
+                <TeamBox
+                    showMoreDetail={false}
+                    showWaitingJoin={true}
+                    showSubBox={true}
+                    borderColor="rgba(0, 163, 255, 0.5)"
+                />
+                <TeamBox
+                    showMoreDetail={false}
+                    showWaitingJoin={true}
+                    showSubBox={true}
+                    borderColor="rgba(231, 137, 255, 0.5)"
+                />
+            </S.BoxDetail>
+            <S.Div>
+                <S.UpImage onClick={() => window.scrollTo(0, 0)}></S.UpImage>
+            </S.Div>
         </div>
     );
 };
