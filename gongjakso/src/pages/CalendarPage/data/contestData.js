ab9fe4d8be91b11edec743af64472fa4a072faa5
Hanseul Lee <lhs010107@gmail.com>
2024-02-11 15:36:16 +0900

    :lipstick: Edit Pages Styles
    #61 캘린더 및 공고 상세페이지 수정

--- gongjakso/src/pages/CalendarPage/data/contestData.js
+++ gongjakso/src/pages/CalendarPage/data/contestData.js
@@ -1,16 +1,24 @@
 let contestData = [
     {
         title: '공모전 마감',
-        color: '#00a2ff5d',
-        textColor: '#00a2ff',
-        date: '2024-01-14',
+        color: '#00a2ff32',
+        textColor: '#00a3ff',
+        date: '2024-02-01',
     },
     {
         title: '공모전 마감',
-        color: '#00a2ff5d',
-        textColor: '#00a2ff',
-        date: '2024-01-17',
+        color: '#00a2ff32',
+        textColor: '#00a3ff',
+        date: '2024-02-17',
+    },
+    {
+        title: '공모전 마감',
+        color: '#00a2ff32',
+        textColor: '#00a3ff',
+        date: '2024-02-19',
     },
 ];
 
 export default contestData;
+
+// 공모전 배경색: #00a2ff32 & 글자색: #00a3ff
