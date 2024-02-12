4d3101d36e3c5824e54d341cbb4a122aee129796
sxxcxng <subin3620@naver.com>
2024-02-12 15:05:37 +0900

    :lipstick: 프로필 나의 정보 api 연결
    #64 프로필 나의 정보 api 연결

--- gongjakso/src/pages/ProfileInfo/MyInfo.jsx
+++ gongjakso/src/pages/ProfileInfo/MyInfo.jsx
@@ -1,7 +1,8 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 import * as S from './MyInfoStyled';
 import majorData from '../../utils/majorData.json';
 import jobData from '../../utils/jobData.json';
+import axios from 'axios';
 
 const groupMajorData = data => {
     return data.reduce((groups, item) => {
@@ -24,6 +25,47 @@ const groupJobData = data => {
 const MyInfo = () => {
     const groupedMajorData = useMemo(() => groupMajorData(majorData), []);
     const groupedJobData = useMemo(() => groupJobData(jobData), []);
+    const accessToken = localStorage.getItem('accessToken');
+    const [name, setName] = useState('');
+    const [status, setStatus] = useState('');
+    const [major, setMajor] = useState('');
+    const [job, setJob] = useState('');
+    const [userInfo, setUserInfo] = useState(null);
+
+    const saveInfo = async () => {
+        const baseURL = 'http://43.200.78.94:8080/';
+
+        try {
+            const response = await axios.put(`${baseURL}api/v1/member`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${accessToken}`,
+                },
+                body: {
+                    name,
+                    status,
+                    major,
+                    job,
+                },
+            });
+            if (response.status === 200) {
+                const userInfo = response.data;
+                console.log(userInfo);
+                setUserInfo(userInfo);
+            } else {
+                console.log('서버로부터의 응답이 예상과 다릅니다.', response);
+            }
+        } catch (error) {
+            console.error('서버 요청 중 오류가 발생했습니다: ', error);
+        }
+    };
+
+    const status_options = [
+        '대학 재학 중',
+        '대학 휴학 중',
+        '취업 준비 중',
+        '기타',
+    ];
 
     return (
         <S.Div>
@@ -34,11 +76,18 @@ const MyInfo = () => {
             <S.Formset>
                 <S.DetailBox>
                     <S.SubTitle>이름</S.SubTitle>
-                    <S.InputField type="text" />
+                    <S.InputField
+                        type="text"
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                    />
                 </S.DetailBox>
                 <S.DetailBox>
                     <S.SubTitle>전공</S.SubTitle>
-                    <S.SelectField>
+                    <S.SelectField
+                        value={major}
+                        onChange={e => setMajor(e.target.value)}
+                    >
                         {Object.entries(groupedMajorData).map(
                             ([group, majors]) => (
                                 <optgroup key={group} label={group}>
@@ -55,7 +104,10 @@ const MyInfo = () => {
 
                 <S.DetailBox>
                     <S.SubTitle>희망 직무</S.SubTitle>
-                    <S.SelectField>
+                    <S.SelectField
+                        value={job}
+                        onChange={e => setJob(e.target.value)}
+                    >
                         {Object.entries(groupedJobData).map(([group, jobs]) => (
                             <optgroup key={group} label={group}>
                                 {jobs.map(job => (
@@ -67,9 +119,23 @@ const MyInfo = () => {
                         ))}
                     </S.SelectField>
                 </S.DetailBox>
+
+                <S.DetailBox>
+                    <S.SubTitle>현재 상태</S.SubTitle>
+                    <S.SelectField
+                        value={status}
+                        onChange={e => setStatus(e.target.value)}
+                    >
+                        {status_options.map(option => (
+                            <option key={option} value={option}>
+                                {option}
+                            </option>
+                        ))}
+                    </S.SelectField>
+                </S.DetailBox>
             </S.Formset>
             <S.Wrapper>
-                <S.SetBox>정보 저장하기</S.SetBox>
+                <S.SetBox onClick={saveInfo}>정보 저장하기</S.SetBox>
             </S.Wrapper>
         </S.Div>
     );
