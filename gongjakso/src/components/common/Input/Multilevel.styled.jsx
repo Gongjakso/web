337d17cde38504aa42e314a40cd27b97ea3f2673
Hanseul Lee <lhs010107@gmail.com>
2024-02-13 14:00:17 +0900

    :lipstick: New Multilevel Dropdown
    새로운 드롭다운 구현

--- gongjakso/src/components/common/Input/Multilevel.styled.jsx
+++ gongjakso/src/components/common/Input/Multilevel.styled.jsx
@@ -5,27 +5,21 @@ export const Title = styled.div`
 `;
 
 export const Dropdown = styled.div`
-    .Dropdown_dropdown__wiOTC
-        .Dropdown_button__eEW4z.Dropdown_button-secondary__9Sb1O {
-        width: 250px;
-        height: 70px;
-        border-radius: 10px;
+    .rnd > button {
         border: 2px solid #c4c4c4;
-        background: white;
-        font-size: ${({ theme }) => theme.fontSize.lg};
+        text-align: left;
         padding: 20px;
+        width: 250px;
+        font-size: ${props => props.theme.fontSize.lg};
+        color: #8c8c8c;
+        border-radius: 10px;
     }
-    .Item_item__Hs3Bb {
-        font-size: ${({ theme }) => theme.fontSize.lg};
+    .rnd__root-menu.rnd__menu {
+        width: 250px !important;
     }
 
-    .Dropdown_dropdown__wiOTC .Dropdown_menu__sSjtr {
-        width: 130px;
-        left: 0;
-        border: 1px solid #c4c4c4;
-    }
-    .Submenu_submenu__hrJ88 {
-        position: absolute;
-        left: 100%;
+    .rnd .rnd__root-menu.rnd__menu .rnd__option .rnd__option-label {
+        font-size: ${props => props.theme.fontSize.lg};
+        padding: 5px;
     }
 `;
