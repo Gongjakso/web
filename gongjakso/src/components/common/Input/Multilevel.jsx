6a9753075c2a2d6d955a04941822f84508dfe14c
Hanseul Lee <lhs010107@gmail.com>
2024-02-13 14:28:32 +0900

    :lipstick: Resolve merge conflict Dropdown
    #71 Merge Dropdown 해결

--- gongjakso/src/components/common/Input/Multilevel.jsx
+++ gongjakso/src/components/common/Input/Multilevel.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-<<<<<<< HEAD
 // import Dropdown from 'react-multilevel-dropdown';
 import * as S from './Multilevel.styled';
 import Down from '../../../assets/images/Down.svg';
@@ -102,66 +101,6 @@ const Multilevel = () => {
                         {title}
                     </button>
                 )}
-=======
-import Dropdown from 'react-multilevel-dropdown';
-import * as S from './Multilevel.styled';
-import Down from '../../../assets/images/Down.svg';
-
-const Multilevel = () => {
-    const [title, setTitle] = useState('지역');
-
-    const city = [
-        '서울',
-        '경기',
-        '인천',
-        '강원',
-        '대전',
-        '세종',
-        '충남',
-        '충북',
-        '부산',
-        '울산',
-        '경남',
-        '경북',
-        '대구',
-        '광주',
-        '전남',
-        '전북',
-        '제주',
-    ];
-
-    const Arrow = () => {
-        return (
-            <S.Title>
-                <img src={Down} />
-            </S.Title>
-        );
-    };
-    return (
-        <S.Dropdown>
-            <Dropdown
-                title={
-                    <>
-                        {title}
-                        <Arrow />
-                    </>
-                }
-            >
-                {city.map((item, index) => (
-                    <Dropdown.Item
-                        key={index}
-                        onClick={() => {
-                            setTitle(item);
-                        }}
-                    >
-                        {item}
-                        <Dropdown.Submenu>
-                            <Dropdown.Item>Subitem 1</Dropdown.Item>
-                            <Dropdown.Item>Subitem 2</Dropdown.Item>
-                        </Dropdown.Submenu>
-                    </Dropdown.Item>
-                ))}
->>>>>>> 05fc67e98f2c136f2e27a413d061bdef7d4d7fed
             </Dropdown>
         </S.Dropdown>
     );
