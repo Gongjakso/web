e0cff44756189eb02ca0cdb32168840e460311dd
chaeyoungwon <epopcy@naver.com>
2024-02-18 20:25:48 +0900

    💄 Update SelectInput UI
    #103 selectinput UI 수정

--- gongjakso/src/components/common/Input/Input.jsx
+++ gongjakso/src/components/common/Input/Input.jsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import * as S from './Input.Styled';
 import { WarningMsg } from './WarningMsg';
+import Down from '../../../assets/images/Down.svg';
+import Up from '../../../assets/images/dropUp.png';
 
 const Input = props => {
     const {
@@ -45,9 +47,35 @@ const SelectInput = props => {
     } = props;
 
     const isLabel = !!label;
+    const [selectedOption, setSelectedOption] = useState('');
+    const [isOpen, setIsOpen] = useState(false);
+    const selectRef = useRef(null); // Ref to select container
+
+    useEffect(() => {
+        const handleClickOutside = event => {
+            if (
+                selectRef.current &&
+                !selectRef.current.contains(event.target)
+            ) {
+                setIsOpen(!isOpen);
+            }
+        };
+        if (isOpen) {
+            window.addEventListener('click', handleClickOutside);
+        }
+
+        return () => {
+            window.removeEventListener('click', handleClickOutside);
+        };
+    }, [isOpen, selectRef]);
+
+    const handleToggleOpen = () => {
+        setIsOpen(!isOpen);
+    };
 
     const handleSelectChange = event => {
         const selectedValue = event.target.value;
+        setSelectedOption(selectedValue);
         onChange(selectedValue); // Call the onChange function with the selected value
     };
     return (
@@ -55,24 +83,30 @@ const SelectInput = props => {
             <S.InputLabel isLabel={isLabel} htmlFor={id}>
                 {label}
             </S.InputLabel>
-            <S.Select
-                name={id}
-                id={id}
-                className={error ? 'warning' : null}
-                {...register(id, registerOptions)}
-                onChange={handleSelectChange}
-            >
-                <option value="" disabled selected hidden>
-                    {placeholder}
-                </option>
-                {selectOptions.map((option, index) => {
-                    return (
-                        <option value={option} key={index}>
-                            {option}
-                        </option>
-                    );
-                })}
-            </S.Select>
+            <S.SelectContainer ref={selectRef}>
+                <S.SelectValue onClick={handleToggleOpen}>
+                    {selectedOption || placeholder}
+                    <S.Arrow src={isOpen ? Up : Down} alt="arrow" />
+                </S.SelectValue>
+                {isOpen && (
+                    <S.OptionList>
+                        {selectOptions.map((option, index) => (
+                            <li
+                                className="option"
+                                key={index}
+                                onClick={() => {
+                                    handleSelectChange({
+                                        target: { value: option },
+                                    });
+                                    handleToggleOpen();
+                                }}
+                            >
+                                {option}
+                            </li>
+                        ))}
+                    </S.OptionList>
+                )}
+            </S.SelectContainer>
         </>
     );
 };
