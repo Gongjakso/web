585ccf5329dc9299e52c354b07a873dfce2e0d44
chaeyoungwon <epopcy@naver.com>
2024-02-19 01:10:45 +0900

    💄 Add Calendar API
    #106 calendar API 연동

--- gongjakso/src/pages/CalendarPage/Calendar.jsx
+++ gongjakso/src/pages/CalendarPage/Calendar.jsx
@@ -1,20 +1,34 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
-import contestData from './data/contestData.js';
-import projectData from './data/projectData.js';
 import * as S from './Calendar.styled.jsx';
 import { getCalendar } from '../../service/calendar_service.js';
 
 const Calendar = () => {
-    let [event] = useState([...contestData, ...projectData]);
+    const [event, setEvent] = useState([]);
+    const [scrapPost, setScrapPost] = useState([]);
 
     useEffect(() => {
-        // ID 수정!!!!
-        // 데이터 : 2월 / 3월
-        getCalendar('105', '2024', '2').then(res => {
-            console.log(res);
-        });
+        const fetchData = async () => {
+            try {
+                const res = await getCalendar('105', '2024', '2');
+                console.log(res);
+
+                const eventTitles = res?.data.scrapPosts.map(post => ({
+                    title: post.title,
+                    color: post.postType ? '#00a2ff32' : '#e789ff32',
+                    textColor: post.postType ? '#00a3ff' : '#e789ff',
+                    date: post.endDate.split('T')[0],
+                }));
+                setScrapPost(res?.data.scrapPosts);
+
+                setEvent(prevEvents => [...prevEvents, ...eventTitles]);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
     }, []);
 
     return (
