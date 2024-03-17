8cc23c8795fb7d5203e5cbec0005e9f851d55d96
HyeonJin <ocn4091@naver.com>
2024-03-17 13:39:16 +0900

    :bug: Login error 해결중
    login error 해결중

--- gongjakso/src/components/Auth/Login.jsx
+++ gongjakso/src/components/Auth/Login.jsx
@@ -5,14 +5,17 @@ const Login = () => {
     const REST_API_KEY = process.env.REACT_APP_JAVASCRIPT_KEY;
     const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
     const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}`;
+    const handleLogin = () => {
+        console.log('Kakao Auth URL:', KAKAO_AUTH_URL);
+        // 여기서 다른 작업을 수행할 수도 있습니다.
+        window.location.replace(`${KAKAO_AUTH_URL}`);
+    };
 
     return (
         <S.LoginContent>
             <S.LoginInfo>
                 3초만에 로그인하고 <br /> 공모전과 프로젝트를 시작해보세요!
-                <a href={KAKAO_AUTH_URL}>
-                    <S.Button />
-                </a>
+                <S.Button onClick={handleLogin} />
             </S.LoginInfo>
             <S.Image />
         </S.LoginContent>
