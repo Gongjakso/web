789cb75d40ecefd7977dd5db2d6e8cf1ac1fd150
HyeonJin <ocn4091@naver.com>
2024-05-01 17:47:57 +0900

    :recycle: Add calendar event
    #161 캘린더 이벤트 클릭시 해당 공고로 이동 기능 구현

--- gongjakso/src/pages/CalendarPage/Calendar.jsx
+++ gongjakso/src/pages/CalendarPage/Calendar.jsx
@@ -3,35 +3,51 @@ import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import * as S from './Calendar.styled.jsx';
 import { getCalendar } from '../../service/calendar_service.js';
+import useCustomNavigate from '../../hooks/useNavigate.js';
 
 const Calendar = () => {
     const [event, setEvent] = useState([]);
     const [currentYear, setCurrentYear] = useState(0);
     const [currentMonth, setCurrentMonth] = useState(0);
+    const navigate = useCustomNavigate();
 
     // fetchData 함수 정의
     const fetchData = async (year, month) => {
         try {
-            const res = await getCalendar('105', year, month);
+            const res = await getCalendar(year, month);
             const scrapPosts = res?.data.scrapPosts || [];
 
+            console.log(scrapPosts);
             const eventTitles = scrapPosts.map(post => ({
+                postId: post.postId,
                 title: post.title,
+                postType: post.postType,
                 color: post.postType ? '#e789ff32' : '#00a2ff32',
                 textColor: post.postType ? '#e789ff' : '#00a3ff',
-                date: post.endDate.split('T')[0],
+                date: post.finishDate.split('T')[0],
             }));
-
+            // console.log(eventTitles);
             setEvent(eventTitles);
         } catch (error) {
             console.error('Error fetching data:', error);
         }
     };
 
+    const handleEventClick = info => {
+        const postId = info.event._def.extendedProps.postId;
+        const postType = info.event._def.extendedProps.postType
+            ? 'contest'
+            : 'project';
+        console.log('Clicked event:', postId, postType);
+        navigate(`/${postType}/${postId}`);
+        // 여기에 클릭된 이벤트에 대한 추가적인 처리 로직을 추가할 수 있습니다.
+    };
+
     useEffect(() => {
         const currentDate = new Date();
         const year = currentDate.getFullYear();
         const month = currentDate.getMonth() + 1;
+        console.log(year, month);
         setCurrentYear(year);
         setCurrentMonth(month);
         fetchData(year.toString(), month.toString()); // fetchData 호출
@@ -48,6 +64,7 @@ const Calendar = () => {
                     return year + '.' + month;
                 }}
                 events={event}
+                eventClick={handleEventClick}
                 headerToolbar={{
                     left: 'prev',
                     center: 'title',
