41d55a576724ee649705d0dd9e60e77b11a3d04c
HyeonJin <ocn4091@naver.com>
2024-05-16 18:02:26 +0900

    :recycle: Refactor Calendar 수정
    #180 캘린더 날짜 입력 수정

---
+++ gongjakso/src/components/common/Calendar/SelectDate.Styled.jsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const CalendarContainer = styled.div`
+    position: relative;
+`;
+
+export const DropdownButton = styled.button`
+    width: 160px;
+    height: 60px;
+    border: 1px solid ${({ theme }) => theme.borderline};
+    border-radius: 15px;
+    padding: 15px;
+    color: var(--festie-gray-800, #3a3a3a);
+    font-size: ${({ theme }) => theme.fontSize.base};
+    color: ${({ theme }) => theme.subFont};
+    font-style: normal;
+    font-weight: 500;
+    line-height: 140%;
+    text-align: start;
+    appearance: none;
+    background-color: white;
+    background-repeat: no-repeat;
+    background-position: right 12px center;
+    background-size: 12px;
+`;
+
+export const CalendarWrapper = styled.div`
+    z-index: 11;
+    position: absolute;
+    top: 100%;
+    left: 0;
+    display: ${props => (props.isOpen ? 'block' : 'none')};
+`;
