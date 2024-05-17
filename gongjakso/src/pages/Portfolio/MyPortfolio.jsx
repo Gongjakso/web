8e78f55335278ac2a8ddb085186ba31dc6606ad9
soobin <subin3620@naver.com>
2024-05-17 21:07:32 +0900

    :lipstick: Add portfolio modal
    #182 portfolio modal 추가

--- gongjakso/src/pages/Portfolio/MyPortfolio.jsx
+++ gongjakso/src/pages/Portfolio/MyPortfolio.jsx
@@ -4,8 +4,13 @@ import axios from 'axios';
 import useCustomNavigate from '../../hooks/useNavigate';
 
 import { getMyInfo } from '../../service/auth_service';
+import { openAlertModal } from '../../features/modal/modalSlice/alertModalSlice';
+import { useDispatch } from 'react-redux';
+import AlertModal from '../../components/common/AlertModal/AlertModal';
 
 const TeamPortfolio = () => {
+    const dispatch = useDispatch();
+
     const [email, setEmail] = useState('');
 
     const goToPage = useCustomNavigate();
@@ -39,7 +44,12 @@ const TeamPortfolio = () => {
 
     const handleEmailSubmit = async () => {
         if (!isValidEmail(email)) {
-            alert('유효한 이메일 주소를 입력해주세요.');
+            dispatch(
+                openAlertModal({
+                    titleContent: '포트폴리오',
+                    modalContent: '유효한 이메일 주소를 입력해주세요.',
+                }),
+            );
             return;
         }
 
@@ -49,12 +59,22 @@ const TeamPortfolio = () => {
             });
 
             if (response.status === 200) {
-                alert('출시 후, 메일 전송 해드리겠습니다!');
+                dispatch(
+                    openAlertModal({
+                        titleContent: '포트폴리오',
+                        modalContent: '출시 후, 메일 전송 해드리겠습니다!',
+                    }),
+                );
             } else {
                 console.log('서버로부터의 응답이 예상과 다릅니다.', response);
             }
         } catch (error) {
-            alert('이미 등록된 이메일입니다.');
+            dispatch(
+                openAlertModal({
+                    titleContent: '포트폴리오',
+                    modalContent: '이미 등록된 이메일입니다.',
+                }),
+            );
             console.error('서버 요청 중 오류가 발생했습니다: ', error);
         }
     };
@@ -62,14 +82,12 @@ const TeamPortfolio = () => {
     return (
         <S.Container>
             <S.Title>나의 포트폴리오</S.Title>
-
             <S.Subtitle>
                 다양한 포트폴리오 양식을 구매하고,<br></br>선배들의 조언까지!
             </S.Subtitle>
             <S.Bluetitle>
                 포트폴리오 탭<br></br>Coming Soon
             </S.Bluetitle>
-
             <S.BottomBox>
                 <S.InputEmail
                     type="email"
@@ -79,6 +97,7 @@ const TeamPortfolio = () => {
                 />
                 <S.CheckBox onClick={handleEmailSubmit}>확인</S.CheckBox>
             </S.BottomBox>
+            <AlertModal />;
         </S.Container>
     );
 };
