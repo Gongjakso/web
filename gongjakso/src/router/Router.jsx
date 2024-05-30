35051aa49df79f62408a336393254efef78465a7
HyeonJin <ocn4091@naver.com>
2024-05-31 03:14:01 +0900

    :recycle: Refactor code
    랜더링이 오래걸리는 문제 해

--- gongjakso/src/router/Router.jsx
+++ gongjakso/src/router/Router.jsx
@@ -1,93 +1,96 @@
+import { React, Suspense, lazy } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import PublicRoutePage from './PublicRoute';
-import PrivateRoutePage from './PrivateRoute';
-import { Suspense } from 'react';
-import HomePage from '../pages/HomePage/HomePage';
-import DefaultLayout from '../Layout/DefaultLayout';
-import ProfilePage from '../pages/ProfilePage/ProfilePage';
-import TeamPart from '../pages/ProfileParticipated/ParticipatedTeam';
-import TeamSupport from '../pages/ProfileApplied/AppliedTeam';
-import TeamPortfolio from '../pages/Portfolio/MyPortfolio';
-import Calendar from '../pages/CalendarPage/Calendar';
-import DetailPageContest from '../pages/DetailPage/DetailPageContest';
-import DetailPageProject from '../pages/DetailPage/DetailPageProject';
-import Login from '../components/Auth/Login';
-import KakaoRedirectPage from '../components/Auth/KakaoRedirectPage';
-import TeamBuildPage from '../pages/TeamBuildPage/TeamBuildPage';
-import PostMainPage from '../pages/PostMainPage/PostMainPage';
-import ProfileRecruit from '../pages/ProfileRecruit/ProfileRecruit';
-import RecruitedTeam from '../pages/ProfileRecruited/RecruitedTeam';
-import MyInfo from '../pages/ProfileInfo/MyInfo';
-import Scrap from '../pages/ScrapPage/Scrap';
-import ScrollToTop from '../pages/HomePage/ScrollToTop';
+const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
+const DefaultLayout = lazy(() => import('../Layout/DefaultLayout'));
+const ProfilePage = lazy(() => import('../pages/ProfilePage/ProfilePage'));
+const TeamPart = lazy(
+    () => import('../pages/ProfileParticipated/ParticipatedTeam'),
+);
+const TeamSupport = lazy(() => import('../pages/ProfileApplied/AppliedTeam'));
+const TeamPortfolio = lazy(() => import('../pages/Portfolio/MyPortfolio'));
+const Calendar = lazy(() => import('../pages/CalendarPage/Calendar'));
+const DetailPageContest = lazy(
+    () => import('../pages/DetailPage/DetailPageContest'),
+);
+const DetailPageProject = lazy(
+    () => import('../pages/DetailPage/DetailPageProject'),
+);
+const Login = lazy(() => import('../components/Auth/Login'));
+const KakaoRedirectPage = lazy(
+    () => import('../components/Auth/KakaoRedirectPage'),
+);
+const TeamBuildPage = lazy(
+    () => import('../pages/TeamBuildPage/TeamBuildPage'),
+);
+const PostMainPage = lazy(() => import('../pages/PostMainPage/PostMainPage'));
+const ProfileRecruit = lazy(
+    () => import('../pages/ProfileRecruit/ProfileRecruit'),
+);
+const RecruitedTeam = lazy(
+    () => import('../pages/ProfileRecruited/RecruitedTeam'),
+);
+const MyInfo = lazy(() => import('../pages/ProfileInfo/MyInfo'));
+const Scrap = lazy(() => import('../pages/ScrapPage/Scrap'));
+const ScrollToTop = lazy(() => import('../pages/HomePage/ScrollToTop'));
 
 const Router = () => {
     return (
-        <BrowserRouter>
-            <ScrollToTop>
-                <Suspense>
+        <Suspense>
+            <BrowserRouter>
+                <ScrollToTop>
                     <Routes>
                         <Route element={<DefaultLayout />}>
-                            <Route path={'/'} element={<HomePage />} />
+                            <Route path="/" element={<HomePage />} />
+                            <Route path="/contest" element={<PostMainPage />} />
+                            <Route path="/project" element={<PostMainPage />} />
+                            <Route path="/calendar" element={<Calendar />} />
                             <Route
-                                path={'/contest'}
-                                element={<PostMainPage />}
-                            />
-                            <Route
-                                path={'/project'}
-                                element={<PostMainPage />}
-                            />
-                            <Route path={'/calendar'} element={<Calendar />} />
-                            <Route
-                                path={'/teambuild'}
+                                path="/teambuild"
                                 element={<TeamBuildPage />}
                             />
+                            <Route path="/profile" element={<ProfilePage />} />
                             <Route
-                                path={'/profile'}
-                                element={<ProfilePage />}
-                            />
-                            <Route
-                                path={'/recruitedTeam'}
+                                path="/recruitedTeam"
                                 element={<RecruitedTeam />}
                             />
                             <Route
-                                path={'/appliedTeam'}
+                                path="/appliedTeam"
                                 element={<TeamSupport />}
                             />
                             <Route
-                                path={'/participatedTeam'}
+                                path="/participatedTeam"
                                 element={<TeamPart />}
                             />
-                            <Route path={'/scrap'} element={<Scrap />} />
+                            <Route path="/scrap" element={<Scrap />} />
                             <Route
-                                path={'/teamPortfolio'}
+                                path="/teamPortfolio"
                                 element={<TeamPortfolio />}
                             />
 
                             <Route
-                                path={'/contest/:id'}
+                                path="/contest/:id"
                                 element={<DetailPageContest />}
                             />
                             <Route
-                                path={'/project/:id'}
+                                path="/project/:id"
                                 element={<DetailPageProject />}
                             />
 
-                            <Route path={'/login'} element={<Login />} />
+                            <Route path="/login" element={<Login />} />
                             <Route
-                                path={'/teamdetail/:id'}
+                                path="/teamdetail/:id"
                                 element={<ProfileRecruit />}
                             />
-                            <Route path={'/myinfo'} element={<MyInfo />} />
+                            <Route path="/myinfo" element={<MyInfo />} />
                             <Route
                                 path="/kakao/callback"
                                 element={<KakaoRedirectPage />}
                             />
                         </Route>
                     </Routes>
-                </Suspense>
-            </ScrollToTop>
-        </BrowserRouter>
+                </ScrollToTop>
+            </BrowserRouter>
+        </Suspense>
     );
 };
 export default Router;
