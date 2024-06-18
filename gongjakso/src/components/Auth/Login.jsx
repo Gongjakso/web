2fbf367440403812e9e7f2eeaf7c04aa69c8372f
HyeonJin <ocn4091@naver.com>
2024-06-18 18:45:33 +0900

    :bug: Edit login redirect URL
    로그인 리다이렉트 url 수정

--- gongjakso/src/components/Auth/Login.jsx
+++ gongjakso/src/components/Auth/Login.jsx
@@ -3,10 +3,14 @@ import * as S from '../Auth/Login.Styled';
 
 const Login = () => {
     const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
-    const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
-    const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}`;
+    // const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
     const handleLogin = () => {
         // 여기서 다른 작업을 수행할 수도 있습니다.
+        const currentUrl = window.location.href;
+        // /login 부분을 제외한 앞부분만 가져옴
+        const baseUrl = currentUrl.replace(/\/login$/, '');
+        const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${baseUrl}/kakao/callback`;
+
         window.location.replace(`${KAKAO_AUTH_URL}`);
     };
 
